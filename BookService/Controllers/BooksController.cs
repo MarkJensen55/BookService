a73a15843fce7c7a81675ce044ce34450891da9d
MarkJensen55 <markjensen@tds.net>
2015-04-08 10:08:07 -0400

    added dto use to bookscontroller

--- BookService/Controllers/BooksController.cs
+++ BookService/Controllers/BooksController.cs
@@ -18,17 +18,51 @@ namespace BookService.Controllers
         private BookServiceContext db = new BookServiceContext();
 
         // GET: api/Books
-        public IQueryable<Book> GetBooks()
+        public IQueryable<BookDTO> GetBooks()
         {
             // extend to get author name immediately & now remove extent
-            return db.Books; //.Include(b => b.Author);
+            //return db.Books; //.Include(b => b.Author);
+
+            // changed to use dto
+            var books = from b in db.Books
+                        select new BookDTO()
+                        {
+                            Id = b.Id,
+                            Title = b.Title,
+                            AuthorName = b.Author.Name
+                        };
+
+            return books;
         }
 
         // GET: api/Books/5
-        [ResponseType(typeof(Book))]
-        public async Task<IHttpActionResult> GetBook(int id)
+        
+        //[ResponseType(typeof(Book))]
+        //public async Task<IHttpActionResult> GetBook(int id)
+        //{
+        //    Book book = await db.Books.FindAsync(id);
+        //    if (book == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return Ok(book);
+        //}
+
+        // changed to use dto
+        [ResponseType(typeof(BookDetailDTO))]
+        public async Task<IHttpActionResult> GetBook (int ID)
         {
-            Book book = await db.Books.FindAsync(id);
+            var book = await db.Books.Include(b => b.Author).Select(b =>
+                new BookDetailDTO()
+            {
+                Id = b.Id,
+                Title=b.Title,
+                Year=b.Year,
+                Price=b.Price,
+                AuthorName=b.Author.Name,
+                Genre=b.Genre
+            }).SingleOrDefaultAsync(b => b.Id == ID);
             if (book == null)
             {
                 return NotFound();
@@ -36,6 +70,7 @@ namespace BookService.Controllers
 
             return Ok(book);
         }
+
 
         // PUT: api/Books/5
         [ResponseType(typeof(void))]
