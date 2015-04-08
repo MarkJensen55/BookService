36724b497c4a5fc2e57f87d916d6108b73e1f7a4
MarkJensen55 <markjensen@tds.net>
2015-04-08 14:20:17 -0400

    added viewmodel function
    also knockout package

---
+++ BookService/Scripts/app.js
@@ -0,0 +1,33 @@
+﻿var ViewModel = function() {
+    var self = this;
+    self.books = ko.obervableArray();
+
+    self.error = ko.Observable();
+
+    var booksUri = 'api/books';
+
+    function ajaxHelper(uri,method,data){
+        self.error(' ');  // clear out errors
+        return $.ajax({
+            type: method,
+            url:uri,
+            datatype: 'json',
+            contentType: 'application/json',
+            data: data ? JSON.stringify(data): null
+        }).fail(function (jqXHR, textStatus, errorThrown) {
+            self.error(errorThrown);
+        });
+    }
+
+    function getAllBooks() {
+        ajaxHelper(booksUri, 'GET').done(function (data) {
+            self.books(data);
+        });
+    }
+
+    // fetch the initial data
+    getAllBooks();
+
+};
+
+ko.applyBindings(new ViewModel());
