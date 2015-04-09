00dcd527eeb9a70d8f9e1ff279a6461d7b1f10cf
MarkJensen55 <markjensen@tds.net>
2015-04-09 13:49:37 -0400

    add getbookdetail to javascript

--- BookService/Scripts/app.js
+++ BookService/Scripts/app.js
@@ -1,8 +1,8 @@
 ﻿var ViewModel = function() {
     var self = this;
     self.books = ko.observableArray();
-
     self.error = ko.observable();
+    self.detail = ko.observable();
 
     var booksUri = '/api/books/';
 
@@ -22,6 +22,12 @@
     function getAllBooks() {
         ajaxHelper(booksUri, 'GET').done(function (data) {
             self.books(data);
+        });
+    }
+
+    self.getBookDetail = function (item) {
+        ajaxHelper(booksUri + item.Id, 'GET').done(function (data) {
+            self.detail(data);
         });
     }
 
